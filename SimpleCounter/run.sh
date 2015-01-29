8d4c7b2439965678312e82a0862e3d893de7e0a5
Gwen Shapira <cshapi@gmail.com>
2015-01-28 16:14:03 -0800

    added shell scripts for demos

---
+++ SimpleCounter/run.sh
@@ -0,0 +1,1 @@
+java -cp target/uber-SimpleCounter-1.0-SNAPSHOT.jar com.shapira.examples.oldproducer.simplecounter.SimpleCounterOldProducer localhost:9092 video1 sync 500 10
---
+++ SimpleCounter/run_params.sh
@@ -0,0 +1,1 @@
+java -cp target/uber-SimpleCounter-1.0-SNAPSHOT.jar com.shapira.examples.oldproducer.simplecounter.SimpleCounterOldProducer "$@"
