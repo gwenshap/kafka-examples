e4089b7aa94a6de5a1061c8176800c49c962c02d
Gwen Shapira <cshapi@gmail.com>
2015-01-28 19:15:04 -0800

    added uber jar and utility scripts

---
+++ SimpleMovingAvg/run.sh
@@ -0,0 +1,1 @@
+java -cp target/uber-SimpleMovingAvg-1.0-SNAPSHOT.jar com.shapira.examples.zkconsumer.simplemovingavg.SimpleMovingAvgZkConsumer localhost:2181 avg video1 10 100000
---
+++ SimpleMovingAvg/run_params.sh
@@ -0,0 +1,1 @@
+java -cp target/uber-SimpleMovingAvg-1.0-SNAPSHOT.jar com.shapira.examples.zkconsumer.simplemovingavg.SimpleMovingAvgZkConsumer "$@"
